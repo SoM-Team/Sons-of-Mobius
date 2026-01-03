0f098e71fbf74b97774ad5f8da52b258935e97ce
Egor Ananyin <ananinegor@gmail.com>
2026-01-03 19:40:34 +0300

    Add script to add basic AI to focuses

---
+++ scripts/add_ai.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import re
+
+pathlist = Path("../common/national_focus").glob("*.*")
+
+ai = r"""    ai_will_do = {
+            base = 1
+        }
+"""
+
+for path in pathlist:
+    with open(path, 'r', encoding='utf-8') as f:
+        code = f.read()
+        ends = []
+        it = re.finditer(r'^\s*focus\s*=\s*\{', code)
+        for focus in it:
+            count = 1
+            for i, c in enumerate(code[focus.end():], start=focus.end()):
+                if c == '{':
+                    count += 1
+                if c == '}':
+                    count -= 1
+                if count == 0:
+                    if re.search(r'ai_will_do', code[focus.end():i]) is None:
+                        ends.append(i)
+                    break
+        for e in ends[::-1]:
+            code = code[0:e] + ai + code[e:]
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(code)
